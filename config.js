97cefcad6e33705ba3c4b5d0a5ce2e8c95406d42
Anh Vu <ducanh03102003@gmail.com>
2023-09-11 14:34:44 -0400

    message

---
+++ config.js
@@ -0,0 +1,17 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐Hé lu cục cưng của anh🖐',
+    introDesc: `Anh có điều này muốn hỏi cưng nè🤫!\t\tCưng nhớ phải trả lời thật lòng nha🤭 `,
+    btnIntro: 'Okiiii anh🥰',
+    title: 'Cho anh cắn miếng chíu chiu hoi được hog? 🤤',
+    desc: 'Em không trả lời hoặc thoát ra, tức là Em đã cho cắn anh rùi đó nha :>> ',
+    btnYes: '💖Có, em hứu sẽ cho anh cắn mà~~~ 🤭💖',
+    btnNo: '💔 Anh bị điên à! 💔 ',
+    question:'Nói cho Anh biết đi vì sao em lại cho anh cắn em i mòa~~~ 🤭🤭🤭',
+    reasonPlaceholder: 'Nghiện anh rùi đúng hog😗😍',
+    btnReply: 'Gởi! 🫣',
+    mess: 'Mẹ em dạy nói điêu không tốt đâu à nghen😥, nghiện anh rùi thì nói i😗',
+    messDesc: '💬💬 Nhắn tin cho anh liền nhớ😉, để anh qua 🥩 cưng liền ó😝 ',
+    btnAccept: 'Nhắn lun cho nóng nè😘, thương cưng nhắm cơ~~~',
+    messLink: 'https://www.facebook.com/ducanh.vu.3910829'
+}
